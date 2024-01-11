6649d08890ce6ace2b7ae9be0641e0d58dfa1884
Maja <maja8709@mail.hansenberg.dk>
2024-01-11 12:34:38 +0100

    opg

--- Opgaver/Program.cs
+++ Opgaver/Program.cs
@@ -4,4 +4,28 @@ Expected Output :
 1 2 3 4 5 6 7 8 9 10
 
 Console.WriteLine("1 2 3 4 5 6 7 8 9 10");
+
+
+3. Write a C# Sharp program to check whether a given number is positive or negative.
+Test Data : 14
+Expected Output :
+14 is a positive number
+
+
+int num;
+Console.WriteLine("\n\n");
+Console.WriteLine("Input a number :");
+num=Convert.ToInt32(Console.ReadLine());
+
+arrays
+
+opg
+Console.WriteLine("hello my name is" + "\n" + "My name is maja" );
+
+
+Console.WriteLine(15 + 17);
 */
+
+Console.WriteLine ("Write a Char (one character)");
+
+char inputChar = Convert.ToChar(Console.ReadLine());
