45876a5e692bbebc70f0140ce61fc8f1e7f188d8
Maja <maja8709@mail.hansenberg.dk>
2024-03-07 09:06:33 +0100

    hjb

--- Opgaver/Program.cs
+++ Opgaver/Program.cs
@@ -1,41 +1,42 @@
-﻿/* opg 1 
-1. Write a program in C# Sharp to display the first 10 natural numbers.
-Expected Output :
-1 2 3 4 5 6 7 8 9 10
-
-Console.WriteLine("1 2 3 4 5 6 7 8 9 10");
-
-
-3. Write a C# Sharp program to check whether a given number is positive or negative.
-Test Data : 14
-Expected Output :
-14 is a positive number
-
-
-int num;
-Console.WriteLine("\n\n");
-Console.WriteLine("Input a number :");
-num=Convert.ToInt32(Console.ReadLine());
-
-arrays
-
-opg
-Console.WriteLine("hello my name is" + "\n" + "My name is maja" );
-
-
-Console.WriteLine(15 + 17);
-
-
-int x = Convert.ToInt32(Console.ReadLine());
-int y = Convert.ToInt32(Console.ReadLine());
-
-if(x == y )
+﻿
+/*class Program
 {
-Console.WriteLine($"x = {x} and y = {y} are equel");
-}
-else
-{
-Console.WriteLine($"x = {x} and y = {y} are not equel");   
+
+    static void Main()
+    {
+        
+        string str= HelloWorld();
+       Console.WriteLine (str);
+
+    }
+
+    static string HelloWorld()
+    {
+        
+        return "mr. poopy butthole";
+    }
+
 }
 */
 
+
+int sum(int x)
+{
+    if (x >= 10)
+    {
+        return 10;
+    }
+    Console.WriteLine($"{x}");
+    if (x <= 9)
+    {
+        return sum(x + 1);
+    }
+    return x;
+}
+
+void start()
+{
+    Console.WriteLine(sum(1));
+}
+
+start();
